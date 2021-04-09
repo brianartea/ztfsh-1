3351255e15e141f0f451b7e882350e1b89f6b6d5
brianartea <bri@expertdesign.cc>
2021-04-08 21:47:34 -0400

    Emmet, ES7 Snippets, Prettier added to VSCODE

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="app">
       <h1>I am the App.js</h1>
+      <h2 className="subtitle">Coolness, and this was done using emmet.</h2>
     </div>
   );
 }
