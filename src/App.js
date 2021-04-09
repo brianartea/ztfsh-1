07a7a85cfdf7290a8ca6eee5e0a9599eb5546dcc
brianartea <bri@expertdesign.cc>
2021-04-08 23:19:29 -0400

    Class based component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import "./App.css";
-import Header from "./Header";
+import React, { Component } from "react";
 
-//Functional Component
-function App() {
-  return (
-    //Bem naming convention = Block Element Modifier
-    <div className="app">
-      <Header />
-      <h1 className="app__title">This is a title</h1>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello world!</h1>
+      </div>
+    );
+  }
 }
 
-export default App;
+// import "./App.css";
+// import Header from "./Header";
+
+// //Functional Component
+// function App() {
+//   return (
+//     //Bem naming convention = Block Element Modifier
+//     <div className="app">
+//       <Header />
+//       <h1 className="app__title">This is a title</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
