7330f8c635c359063c14de8f4d29aab30ba7298e
brianartea <bri@expertdesign.cc>
2021-04-08 21:52:50 -0400

    JSX Explained

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <h1>I am the App.js</h1>
+      <h1>I am the App.js with JSX</h1>
       <h3 className="subtitle">Coolness, and this was done using emmet</h3>
     </div>
   );
