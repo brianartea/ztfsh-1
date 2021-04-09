a19cf2aaff0070d3b8a445b50d0574605970add8
brianartea <bri@expertdesign.cc>
2021-04-08 22:28:03 -0400

    Bem - Bloxk Element Modifier

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import "./App.css";
 
 function App() {
   const name = "Brian";
+  const isMale = true;
+
+  function getGreeting(user) {
+    if (user) {
+      return <h1>Hello, {user}!</h1>;
+    }
+    return <h1>Hello, Stranger</h1>;
+  }
+
+  //Bem = Block Element Modifier
 
   return (
     <div className="app">
+      {getGreeting()}
       <h1>I am the App.js with JSX</h1>
+      <h2>You are a {isMale ? "Male" : "Female"}</h2>
       <h3 className="subtitle">
         Hello {name}, hope you are having a good day.
       </h3>
