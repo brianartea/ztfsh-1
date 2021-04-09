cc0fc5e532339e8b6ebe89a8fee02d48b4780729
brianartea <bri@expertdesign.cc>
2021-04-08 21:50:12 -0400

    Installed gitlens

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="app">
       <h1>I am the App.js</h1>
-      <h2 className="subtitle">Coolness, and this was done using emmet.</h2>
+      <h3 className="subtitle">Coolness, and this was done using emmet</h3>
     </div>
   );
 }
