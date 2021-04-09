168c57336084fffb02fcfdebbeb194336e05cd51
brianartea <bri@expertdesign.cc>
2021-04-09 19:59:37 -0400

    Passed down 3 props from App.js

--- src/Product.js
+++ src/Product.js
@@ -3,7 +3,7 @@ import React from "react";
 function Product(props) {
   return (
     <div>
-      <h3>This is the Product.js with 3 props</h3>
+      <h3>This is the Product.js with 3 props from App.js</h3>
       <h1>{props.name}</h1>
       <h2>{props.description}</h2>
       <h3>${props.price}</h3>
