78fa63296a460489823e4f541c8bcdefea73cb26
brianartea <bri@expertdesign.cc>
2021-04-08 23:20:25 -0400

    Functional component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
-import React, { Component } from "react";
+import "./App.css";
+import Header from "./Header";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello world!</h1>
-      </div>
-    );
-  }
+//Functional Component
+function App() {
+  return (
+    //Bem naming convention = Block Element Modifier
+    <div className="app">
+      <Header />
+      <h1 className="app__title">This is a title</h1>
+    </div>
+  );
 }
 
-// import "./App.css";
-// import Header from "./Header";
-
-// //Functional Component
-// function App() {
-//   return (
-//     //Bem naming convention = Block Element Modifier
-//     <div className="app">
-//       <Header />
-//       <h1 className="app__title">This is a title</h1>
-//     </div>
-//   );
-// }
-
-// export default App;
+export default App;
