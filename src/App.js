ce07e782e03e09cfe096efb211ecfa8bd8d284af
brianartea <bri@expertdesign.cc>
2021-04-09 00:03:18 -0400

    Learning State in a functional component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1); // CORRECT
+  };
+
+  const decrement = () => {
+    setCount(count - 1); // CORRECT
+  };
+
   return (
     <div>
-      <h1>Refactored Class component to a Functional component</h1>
+      <h3>Counter app</h3>
+      <p>The count is: {count}</p>
+      <button onClick={decrement}>-</button>
+      <button onClick={increment}>+</button>
     </div>
   );
 }
