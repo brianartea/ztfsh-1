292340f07d2bd1585c130302dd711a90ffdc55f1
brianartea <bri@expertdesign.cc>
2021-04-10 10:16:14 -0400

    CSS Modules and CSS variables

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from "react";
 import Product from "./Product";
-import "./App.module.css";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className="app">
-      <h1 className="app__heading">App Component</h1>
-      <h2 className="app__error">An error occured</h2>
+    <div className={styles.app}>
+      <h1 className={styles.heading}>The App</h1>
+      <h2 className={styles.error}>An error occured</h2>
       <Product
         comp="Product 1"
         name="Google Home"
         description="Your AI assistant"
         price={59.99}
       />
-      <hr />
       <Product
         comp="Product 2"
         name="iPhone 12 Pro Max"
         description="The best iPhone"
         price={1299.99}
       />
-      <hr />
       <Product
         comp="Product 3"
         name="Test Product"
