6ba3eccc166572c7724b5395ac540472cc8cf69e
brianartea <bri@expertdesign.cc>
2021-04-09 20:09:25 -0400

    Passing 3 props from App.js to Product.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,24 @@ function App() {
       <h1>App Component</h1>
       <hr />
       <Product
-        name="Amazon Echo"
+        comp="Product 1"
+        name="Google Home"
         description="Your AI assistant"
         price={59.99}
+      />
+      <hr />
+      <Product
+        comp="Product 2"
+        name="iPhone 12 Pro Max"
+        description="The best iPhone"
+        price={1299.99}
+      />
+      <hr />
+      <Product
+        comp="Product 3"
+        name="Test Product"
+        description="Te3st description"
+        price={1100.99}
       />
     </div>
   );
--- src/Product.js
+++ src/Product.js
@@ -4,6 +4,7 @@ function Product(props) {
   return (
     <div>
       <h3>This is the Product.js with 3 props from App.js</h3>
+      <h4>{props.comp}</h4>
       <h1>{props.name}</h1>
       <h2>{props.description}</h2>
       <h3>${props.price}</h3>
